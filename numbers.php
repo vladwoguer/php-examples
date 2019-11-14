c72f09a8e7a3f4684fad5b355c3a5d64967cde1a
vladwoguer.bezerra <vladwoguer.bezerra@virtus.ufcg.edu.br>
2019-11-14 17:39:13 -0300

    First examples

---
+++ numbers.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo 2.0 * 2;
+echo "\n";
+//Decimal
+echo 2;
+echo "\n";
+//Octal
+echo 02;
+echo "\n";
+//Hexadecimal
+echo 0x2;
+echo "\n";
+var_dump(is_int(2));
+echo "\n";
+var_dump(is_long(2));
+
+?>  
+
+</body>
+</html>
